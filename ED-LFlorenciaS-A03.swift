f3dbc26000cfcf22fb1b3ae8e16aa20defbf0c1f
AlvaroFlorencia <A01411537@itesm.mx>
2017-08-17 21:15:47 -0500

    Add files via upload

--- ED-LFlorenciaS-A03.swift
+++ ED-LFlorenciaS-A03.swift
@@ -3,7 +3,7 @@ import Foundation
 class Busquedas 
 {
 var vector: [Int] = [] //Atributo
-func CrearVector(tamaño:Int)->Int
+func CrearVector(tamaño:Int)->[Int]
 {
     var vector:[Int] = [Int]()
     for _ in 0...(tamaño-1)  //se corre el for y se va llenando  el array con datos random ,el %100 es para que aparezcan números enteros
@@ -12,19 +12,20 @@ func CrearVector(tamaño:Int)->Int
         }
         return vector
         }
-func  getVector()-> [Int]  //Getter
+func  getVector()->[Int]   //Getter
 {
 return self.vector
 }
 
-    func MostrarVector(vector:[Int]) 
+    func MostrarVector() 
     {
-    let n:Int = self.vector.count-1 //-1 para evitar un excepción
+    let valor:Int = self.vector.count-1 //-1 para evitar un excepción
     var string = "Valores del array:"   
-    for(valor) in 0...n
+    for(valor) in 0...valor
 {
-    string+=String(self.vector[valor])  //Se recorre el for y se va imprimiendo
-    string+=" , "
+    string+=String(self.vector[valor])
+	string+=" , "//Se recorre el for y se va imprimiendo
+    
     }
     print(string)
     }
@@ -33,35 +34,35 @@ return self.vector
    {
     self.vector = vector
     }
-    func MostrarF(n: Int) //Parametro entero
+    func MostrarF(valor: Int) //Parametro entero
 	{
-        if(n==(-1))
+        if(valor==(-1))
 		{
 			print("El dato no se encuetra dentro del vector")    //Si recibe -1 de clases hijas quiere decri que no se encuentra en el vector
 		}
         else
         {
-			print("El dato  se encuentra dentro del  vector + (n)") //Si es otro número se devuelve la posición
+			print("El dato  se encuentra dentro del  vector",valor) //Si es otro número se devuelve la posición
 		}
     }
 
     
-    func busqueda(n: Int)->Int
+    func busqueda(valor: Int)->Int
     {
     preconditionFailure("Metodo abstracto a utilizar") //Método abstracto
     }
     }
-class binaria: Busquedas
+class binaria: Busquedas,ordenamiento
 {
  override func busqueda(valor: Int) -> Int   
     {
 var inicio: Int = 0
-var fin: Int = self.vector.count-1    //Atributos inicio,medio y fin
+var fin: Int = self.vector.count-1    //Atributos 
 var centro: Int = (inicio+fin)/2  
 	
 while(inicio < fin)
     {
-var posicionCentro:Int = self.vector[centro] 
+let posicionCentro:Int = self.vector[centro] 
 if(posicionCentro == valor) //Si posición es igual al valor se regresa centro
 {
 return centro 
@@ -81,32 +82,61 @@ centro = (inicio + fin)/2;  //De lo contrario el incio sera el centro por que es
      }  
     }
 return -1
-     }
+     
+	}
+	func seleccion (vector: [Int]) ->[Int]
+	{
+		var array : [Int] = vector //Cree otra variable para guardar el valor en una variblae
+		var auxiliar : Int
+		for i in 0...vector.count-1 //Se coore el primer arreglo con posición en i
+		{
+			var menor :Int = i;    //El valor menor se vuelve i
+            for (j) in 1...vector.count-1  //Se coore j
+			{
+			if(vector[j]<array[menor]){  //si el valor en j es menor a la posición de menor
+menor = j;
 }
+}
+			auxiliar = array [i]   //auxiliar se convierte en la posición en i
+array [i] = array [menor]   //la posición en i se vuelve menor
+array [menor] = auxiliar    //Y menor se guarda en la posición auxiliar
+    }
+return vector            //Se devuelve el vector
+}
+}
+
+
 
 class secuencial: Busquedas
 {
     override func busqueda(valor: Int) -> Int
     {
-    let n:Int = self.vector.count-1 //Se recorre el arreglo si el valor es igual a la posición se regresa el valor de la posición si no se regresa un menos uno
-    for i in 0...n
+    let i:Int = self.vector.count-1 //Se recorre el arreglo si el valor es igual a la posición se regresa el valor de la posición si no se regresa un menos uno
+    for i in 0...valor
 {
-            if(self.vector[i] == valor)
-			{
-				return i
-			}
-        }
-        return -1
-    }
+   if(self.vector[i] == valor) // si se encuentra el valor se regresa la posición
+{
+	return i
 }
-//
-var vector: [Int] = [1,2,3,4,5,6,7,8,9] //Arreglo en forma ordenada para pueda funcionar el binario
-var binario1: binary = binary()
-var secuencial1: secuencial = secuencial()
-secuencial1.setVector(array: secuencial1.setVector(n: 30))
-binario1.setVector(array: vector)
-secuencial1.mostrarF(n: secuencial1.busqueda(item: 5))
-binario1.mostrarF(n: binario1.busqueda(item:5))
+  }
+        return -1
+   }
+}
+protocol ordenamiento //Protocolo como interfaz en Java
+{
+	 func seleccion (vector : [Int]) -> [Int]  //
+}
+//Main
 
+
+var secuential: secuencial = secuencial()
+secuential.setVector(vector: secuential.CrearVector(tamaño: 50))
+secuential.MostrarVector()
+secuential.MostrarF(valor: secuential.busqueda(valor: 77))
+var vector1: [Int] = [6,8,9,14,11,78,21,42,45]
+var binario: binaria = binaria()
+binario.setVector(vector: vector1)
+binario.MostrarVector()
+binario.MostrarF(valor: binario.busqueda(valor:14))
 
 
