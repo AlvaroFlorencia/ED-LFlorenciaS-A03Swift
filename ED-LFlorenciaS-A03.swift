2ff950fdd16da769fddd0936ec5fd5e3ae80f0ce
AlvaroFlorencia <A01411537@itesm.mx>
2017-08-28 23:43:30 -0500

    Add files via upload

--- ED-LFlorenciaS-A03.swift
+++ ED-LFlorenciaS-A03.swift
@@ -84,6 +84,143 @@ centro = (inicio + fin)/2;  //De lo contrario el incio sera el centro por que es
 return -1
      
 	}
+func intercambio(p1:Int, p2:Int)  //Función intercambio recibe 2 puntos
+	{
+let auxiliar = self.vector[p1]
+self.vector[p1] = self.vector[p2]  //Hace los intercambios de la mayoría de los metódos de ordenamiento
+self.vector[p2]=auxiliar
+	func inserccion()
+	{
+var array:[Int]=self.vector 
+var auxiliar : Int 
+var j : Int = 0
+var k : Int
+for i in 1...vector.count-1
+{
+k = i
+auxiliar=array[]
+k=k-1
+while(j>=0 && array[j]>auxiliar)
+{
+array=array
+j=j-1
+}
+array]=auxiliar
+}
+self.vector = auxiliar
+}
+func burbuja()
+{
+var array: [Int]=self.array         
+for i in 0...array.count-1{
+for j in 0...array.count-i-1
+{
+	if(array[j+1]<array[j])
+{
+intercambio(p1:j+1,p2:j)
+}
+}
+  }
+self.vector = vector
+}
+func quickSort(inicio:Int, fin:Int) 
+{
+var vector : [Int] = self.vector
+var i: Int = inicio
+var j: Int = fin
+let pivot = vector[inicio]
+while(i<=j)
+{
+while(vector[i]<=pivot && i<j)
+	{
+		i=i+1
+}	while(vector[j]>pivot)
+{
+	j=j-1
+}
+if(i<j)
+	{
+intercambio(i,j)
+i=i+1
+j=j-1
+	}
+}
+vector[inicio] = vector [j]
+vector [j] = pivot
+if(inicio<j-1)
+{
+quickSort(inicio:inicio, fin:j-1)
+}
+if(i<fin)
+{
+quickSort(inicio:j+1, fin:fin)
+}
+self.vector = vector
+}
+func quickSort (incio:Int, termino:Int) 
+{
+var array : [Int] = self.array
+var i: Int = inicio
+var j: Int = termino
+let pivote = array[inicio]
+while(i<=j){
+while(array[i]<=pivote && i<j)
+	{
+i=i+1
+	}
+while(array[j]>pivote)
+{
+j=j-1
+}
+if(i<j)
+{
+intercambio(p1:j,p2:i)
+i=i+1
+j=j-1
+}
+}
+array[inicio] = array [j]
+array [j] = pivote
+if(inicio<j-1)
+{
+quickSort(inicio:inicio, termino:j-1)
+}
+if(i<termino)
+{
+quickSort(inicio:j+1, termino:termino)
+}
+self.array = array
+	}
+func mergeSort (inicio:Int, termino:Int) {   //Se reciben 2 posiciones 
+var array : [Int] = self.array     //Se crea el array
+if(termino-inicio =>0) {//Si el termino -incio es igual o mayor a cero es decir que existe un elemento y no es necesario ordenar
+}
+else{
+let medio = (inicio+termino)/2  //Se divide entre 2 para encotrar el de enmedio
+mergeSort(inicio:inicio, termino:medio)  //Se utiliza la recursividad para ordenar de nuevo los arrays adyacentes
+mergeSort(inicio:medio,termino:termino)
+let posicion1: Int = inicio
+let posicion2: Int = medio  //Se definen las posicions
+let posicion3: Int = 0
+var arrayAuxiliar : [Int] = []
+while(posicion1<medio && posicion2<termino){  //Mientras la posicion inicial es menor que la de enmedio y la posicion 2 es menor que la de temrino se entra al ciclo
+if(posicion1<medio && posicion2<termino){  //Si la posisicion 1 es menor que la de enmedio y la posicion 2 es menor que la del temrino se sigue ingresando y buscando
+if(array[posicion3+1]<array[posicion2+1]){    //Si la posición 3 siguientes es menor que la posición 2 siguiente
+arrayAuxiliar[posicion3+1]=array[posicion1+1]  //Si la posición 3 siguientes es igual a la posición 1 siguiente
+}
+else{
+arrayAuxiliar[posicion3+1]=array[posicion2+1] //La posicion 3 siguiente  es igual a la siguiente posicion 2 
+}
+}else if (posicion1<medio){
+arrayAuxiliar[posicion3+1]=(arrayAuxiliar[posicion1]) //Si no la posicion 3 siguiente es la posición 1
+	}
+	else{
+arrayAuxiliar[posicion3+1] = arrayAuxiliar[posicion2+1] //Si no  la siguiente posición 3 es la siguinete posición 2
+}
+}
+}
+self.array = array
+}
 	func seleccion (vector: [Int]) ->[Int]
 	{
 		var array : [Int] = vector //Cree otra variable para guardar el valor en una variblae
@@ -107,7 +244,11 @@ return vector            //Se devuelve el vector
 
 protocol ordenamiento //Protocolo como interfaz en Java
 {
-	 func seleccion (vector : [Int]) -> [Int]  //
+	 func seleccion (vector : [Int]) -> [Int]
+	func burbuja
+	func inserccion()
+	func mergeSort(inicio:Int, fin:Int)
+	func quickSort(inicio:Int, fin:Int)//
 }
 
 class secuencial: Busquedas
